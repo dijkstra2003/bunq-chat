8e57e01a0908a107171a9e443f1d380c266cda4c
Luc Dijsktra <dijkstra2003@gmail.com>
2019-11-28 15:32:16 +0100

    login request to retrieve users

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -4,23 +4,53 @@ import './login.css'
 import logo from '../../assets/bunq-logo.svg';
 
 class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoading: true,
+            users: []
+        }
+    }
+    
+    componentDidMount() {
+        console.log(this.state.isLoading);
+        fetch("http://assignment.bunq.com/users")
+        .then(res => res.json())
+        .then(
+            (result) => {
+                this.setState({
+                    isLoading: false,
+                    users: result
+                });
+                console.log(this.state.isLoading)
+            },
+            (error) => {
+                this.setState({
+                    isLoading: false,
+                    error: error
+                })
+            }
+        )
+    }
+    
     render() {
         return (
             <div className="login-wrapper">
                 <div className="login">
                     <img src={logo} className="bung-logo" alt="bunq logo"></img>
-                    <Dropdown>
+                    {this.state.isLoading ? (<p>loading users....</p>) : (
+                        <Dropdown>
                         <Dropdown.Toggle variant="secondary">
                             choose a user
                         </Dropdown.Toggle>
                         <Dropdown.Menu>
-                            <Dropdown.Item>Wessel</Dropdown.Item>
-                            <Dropdown.Item>Luc</Dropdown.Item>
-                            <Dropdown.Item>Rian</Dropdown.Item>
-                            <Dropdown.Item>Josh</Dropdown.Item>
-                            <Dropdown.Item>Denzel</Dropdown.Item>
+                            {this.state.users.map((value, index) => {
+                                return <Dropdown.Item key={index}>{value.name}</Dropdown.Item>
+                            })}
                         </Dropdown.Menu>
                     </Dropdown>
+                    )}
                 </div>
             </div>
         )
