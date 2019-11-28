c9ecf84773bda37f7463a35611d867f612f7bd1d
Luc Dijsktra <dijkstra2003@gmail.com>
2019-11-28 14:50:41 +0100

    started with the app

---
+++ src/components/login/login.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Dropdown from 'react-bootstrap/Dropdown';
+import './login.css'
+import logo from '../../assets/vectorpaint.svg';
+
+class Login extends React.Component {
+    render() {
+        return (
+            <div className="login-wrapper">
+                <div className="login">
+                    <img src={logo} className="bung-logo" alt="bunq logo"></img>
+                    <Dropdown>
+                        <Dropdown.Toggle variant="secondary">
+                            choose a user
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            <Dropdown.Item>Wessel</Dropdown.Item>
+                            <Dropdown.Item>Luc</Dropdown.Item>
+                            <Dropdown.Item>Rian</Dropdown.Item>
+                            <Dropdown.Item>Josh</Dropdown.Item>
+                            <Dropdown.Item>Denzel</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Login;
